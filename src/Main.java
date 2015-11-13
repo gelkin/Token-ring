137d990ffd3d4453ef2405ed3060156ffa49e3ce
Martoon-00 <Martoon_00@mail.ru>
2015-11-14 01:09:47 +0300

    Bug with receiving fixed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-import token.ring.NodeContext;
+import sender.listeners.ReplyProtocol;
+import sender.main.MessageSender;
+import token.ring.message.*;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 
 public class Main {
@@ -9,10 +13,16 @@ public class Main {
 //        Collections.list(NetworkInterface.getNetworkInterfaces())
 //                .forEach(System.out::println);
 
-        try (NodeContext nodeContext = new NodeContext(NetworkInterface.getByName("wlan0"), 1247)) {
-            nodeContext.initiate();
-            int stopComputation = System.in.read();
-        }
+        MessageSender sender = new MessageSender(NetworkInterface.getByName("wlan0"), 1247);
+        sender.registerReplyProtocol(ReplyProtocol.of(AmCandidateMsg.class, amCandidateMsg -> new AmSuperiorCandidateMsg()));
+        sender.registerReplyProtocol(ReplyProtocol.of(LostTokenMsg.class, lostTokenMsg -> new RecentlyHeardTokenMsg()));
+        sender.send(new InetSocketAddress(InetAddress.getByName("192.168.0.103"), 1247), new AmCandidateMsg(null), MessageSender.DispatchType.UDP, 1000, (a, b) -> System.out.println("lol"));
+
+        sender.unfreeze();
+//        try (NodeContext nodeContext = new NodeContext(NetworkInterface.getByName("wlan0"), 1247)) {
+//            nodeContext.initiate();
+//            int stopComputation = System.in.read();
+//        }
     }
 
 }
