c9b67c8ed13e7ffa4eebc8b6b1ce3dc38dce6707
Martoon-00 <Martoon_00@mail.ru>
2015-11-14 03:29:32 +0300

    Switching to another port

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import token.ring.NodeContext;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.NetworkInterface;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,13 +13,35 @@ public class Main {
 //        Collections.list(NetworkInterface.getNetworkInterfaces())
 //                .forEach(System.out::println);
 
-//        MessageSender sender = new MessageSender(NetworkInterface.getByName("wlan0"), 1247);
-//        sender.unfreeze();
-//        sender.broadcast(new LostTokenMsg());
-
         try (NodeContext nodeContext = new NodeContext(NetworkInterface.getByName("wlan0"), 1247)) {
             nodeContext.initiate();
-            int stopComputation = System.in.read();
+
+            readCommands(nodeContext);
+        }
+    }
+
+    private static void readCommands(NodeContext nodeCtx) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            if ("stop".equals(line))
+                break;
+
+
+            Matcher matcher = Pattern.compile("port (\\d+)").matcher(line);
+            if (matcher.matches()) {
+                try {
+                    int port = Integer.parseInt(matcher.group(1));
+                    if (port <= 0) {
+                        System.out.println("Bad port");
+                        break;
+                    }
+                    nodeCtx.sender.changePorts(port);
+                } catch (NumberFormatException e) {
+                    System.out.println("Bad port");
+                }
+            }
         }
     }
 
